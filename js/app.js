50ef5be5b4aea0a7284ad5fb05d7524957d374a2
zeirwayne <zeirwayne@github.com>
2019-06-10 14:21:05 +0800

    completed styling

--- js/app.js
+++ js/app.js
@@ -1,13 +1,53 @@
 const projects = document.querySelector("#projects");
+const preloader = document.querySelector("#preloader");
+const greeting = document.querySelector(".greeting");
+const part = document.querySelector(".part");
+const story = document.querySelector(".story");
 
+/* hide the preloader after load*/
+window.addEventListener("load", () => {
+  preloader.classList.add("preloader-done");
+});
+
+/* bg changer in projects */
 function bgChanger() {
   if (this.scrollY > this.innerHeight / 2 + this.innerHeight / 4) {
     projects.classList.add("bg-transition");
-    console.log("add");
   } else {
     projects.classList.remove("bg-transition");
-    console.log("rm");
   }
 }
 
-window.addEventListener("scroll", bgChanger);
+/* slide triger in about*/
+
+const textSlider = element => {
+  if (this.scrollY > document.body.scrollHeight / 2) {
+    element.classList.add("slide");
+  } else {
+    element.classList.remove("slide");
+    console.log("asdasdsadf");
+  }
+};
+
+window.addEventListener("scroll", function() {
+  bgChanger();
+  textSlider(greeting);
+  textSlider(part);
+  textSlider(story);
+});
+
+/* smooth scrolling*/
+
+$('a[href*="#"]').on("click", function(e) {
+  e.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top
+    },
+    500,
+    "linear"
+  );
+});
+
+/* sending email */
